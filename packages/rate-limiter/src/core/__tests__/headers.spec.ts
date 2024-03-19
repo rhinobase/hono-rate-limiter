ec920b4fb4ad7e2c931cc38e8b35ac0669ba193e
Aditya Mathur <mathuraditya724@gmail.com>
2024-03-19 11:53:54 +0530

    fix: corrected github action for testing

--- packages/rate-limiter/src/core/__tests__/headers.spec.ts
+++ packages/rate-limiter/src/core/__tests__/headers.spec.ts
@@ -19,7 +19,7 @@ describe("headers test", () => {
       }),
     );
 
-    const res = await app.request("/");
+    const res = await testClient(app).index.$get();
 
     expect(res.headers.get("ratelimit-policy")).toBe("5;w=60");
     expect(res.headers.get("ratelimit-limit")).toBe("5");
