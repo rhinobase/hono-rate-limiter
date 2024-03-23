9aa3a34642c73b88d719d86b99b19fb28e18b983
Aditya Mathur <57684218+MathurAditya724@users.noreply.github.com>
2024-03-23 17:00:08 +0530

    chore(sandbox): resolved errors

--- apps/sandbox/api/main.ts
+++ apps/sandbox/api/main.ts
@@ -1,23 +1,27 @@
-import { Hono } from "hono";
-// import { type RateLimitInfo, rateLimiter } from "hono-rate-limiter";
-import { logger } from "hono/logger";
-// import Page from "./Page";
+// import { Hono } from "hono";
+// // import { type RateLimitInfo, rateLimiter } from "hono-rate-limiter";
+// import { logger } from "hono/logger";
+// // import Page from "./Page";
 
-// Init the app
-export const app = new Hono();
+// // Init the app
+// export const app = new Hono();
 
-// Adding the rate limitter
-app.use(
-  logger(),
-  // rateLimiter({
-  //   windowMs: 10_000,
-  //   limit: 10,
-  //   // store: new RedisStore({
-  //   //   sendCommand: (...args: string[]) => kv.eval(...args),
-  //   // }),
-  //   handler: (_, next) => next(),
-  // }),
-);
+// // Adding the rate limitter
+// app.use(
+//   logger()
+//   // rateLimiter({
+//   //   windowMs: 10_000,
+//   //   limit: 10,
+//   //   // store: new RedisStore({
+//   //   //   sendCommand: (...args: string[]) => kv.eval(...args),
+//   //   // }),
+//   //   handler: (_, next) => next(),
+//   // }),
+// );
 
-// Routes
-app.all("*", (c) => c.text("hello"));
+// // Routes
+// app.get("/", (c) =>
+//   c.html(
+//     <Page info={{ limit: 5, used: 1, remaining: 4, resetTime: new Date() }} />
+//   )
+// );
