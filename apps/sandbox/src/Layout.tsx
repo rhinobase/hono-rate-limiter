ee0f5c02e38e63bc9d409a105e3347b2af2dc496
Gajendra-singh-r <137156034+Gajendra-singh-r@users.noreply.github.com>
2024-03-26 15:50:06 +0530

    feat(sandbox): added seo

--- apps/sandbox/src/Layout.tsx
+++ apps/sandbox/src/Layout.tsx
@@ -38,11 +38,47 @@ export const Layout: FC<PropsWithChildren> = (props: PropsWithChildren) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <title>Hono Rate Limiter + Vercel KV</title>
+        <meta
+          name="description"
+          content="Rate limiting middleware for Honojs. Use to limit repeated requests to public APIs and/or endpoints such as password reset."
+        />
+        <meta name="author" content="Rhinobase Team" />
+        <meta
+          name="keywords"
+          content="hono,api,middleware,rest-api,rate-limiting,rate-limiter,honojs"
+        />
+        <meta name="category" content="technology" />
+        <meta property="og:title" content="Hono Rate Limiter + Vercel KV" />
+        <meta
+          property="og:description"
+          content="Rate limiting middleware for Honojs."
+        />
+        <meta property="og:url" content="https://" />
+        <meta property="og:site_name" content="hono-rate-limiter-demo" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:image" content="https://rafty.rhinobase.io/api/og" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content="@rhinobaseio" />
+        <meta name="twitter:title" content="Hono Rate Limiter + Vercel KV" />
+        <meta
+          name="twitter:description"
+          content="Rate limiting middleware for Honojs."
+        />
+        <meta name="twitter:image:width" content="1200" />
+        <meta name="twitter:image:height" content="630" />
+        <meta
+          name="twitter:image"
+          content="https://rafty.rhinobase.io/api/og"
+        />
+        <meta name="theme-color" content="#09090b" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap"
           rel="stylesheet"
         />
-        <title>Hono Rate Limiter + Vercel KV</title>
         <Style />
       </head>
       <body class={globalClasses}>
